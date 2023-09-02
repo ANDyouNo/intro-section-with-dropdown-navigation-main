cffccab063b5521ddd2d327856f5ceac612526a0
ANDyouNo <younofl312w@gmail.com>
2023-09-02 19:55:00 +0300

    adaptive header add burger menu button

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  let menuBtn = document.getElementById('menu');
+  let menuWindow = document.querySelector('.menu')
+  menuBtn.classList.toggle('active');
+  // menuWindow.classList.toggle('none')
+}
